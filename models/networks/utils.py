abaa2cd7aca4f0793a5201bc4606bc61165e800b
tinasgong <tinagong@college.harvard.edu>
2024-11-25 14:00:32 -0500

    gpu

--- models/networks/utils.py
+++ models/networks/utils.py
@@ -87,9 +87,9 @@ class unetConv2(nn.Module):
             init_weights(m, init_type='kaiming')
 
     def forward(self, inputs):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        inputs = inputs.to(device)
-        x = inputs
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # inputs = inputs.to(device)
+        x = inputs.cuda()
         for i in range(1, self.n+1):
             conv = getattr(self, 'conv%d'%i)
             x = conv(x)
