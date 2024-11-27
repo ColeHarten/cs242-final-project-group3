178911e8d8fe492e1de3c69232819fa5a534930b
tinasgong <tinagong@college.harvard.edu>
2024-11-27 17:33:24 -0500

    re-add slurm script

---
+++ run_train_seg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH -c 16                # Number of cores (-c) 
+#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb
+#SBATCH -t 1-00:00           # Runtime in D-HH:MM, minimum of 10 minutes 
+#SBATCH -p gpu               # Partition to submit to 
+#SBATCH --mem=128G 
+#SBATCH -o train_%j.out   # File to which STDOUT will be written, %j inserts $
+#SBATCH -e train_%j.err   # File to which STDERR will be written, %j inserts $
+
+module load python/3.10.12-fasrc01
+source activate NNBP-OSD
+
+python3 train_segmentation.py
