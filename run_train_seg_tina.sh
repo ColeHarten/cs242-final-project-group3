ed950de009dc7ca5caa5f85cc05e2a5e714e77c7
tinasgong <christinagong487@gmail.com>
2024-12-01 14:17:31 -0500

    experiement changes from tina

--- run_train_seg_tina.sh
+++ run_train_seg_tina.sh
@@ -3,15 +3,16 @@
 #SBATCH -t 1-12:59           # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH --mem=99G           # Memory pool for all CPU cores
 #SBATCH --gres=gpu:1
-#SBATCH -p gpu_requeue
-#SBATCH -o train_%j.out
-#SBATCH -e train_%j.err 
+#SBATCH -p gpu
+#SBATCH -o earlyexit_train_%j.out
+#SBATCH -e earlyexit_train_%j.err 
 #SBATCH --exclude=holygpu8a31305,holygpu8a31402,holygpu8a31202,holygpu8a29106,holygpu8a25104
 
 # Load software
 module load cuda/11.8.0-fasrc01
 module load cudnn/8.8.0.121_cuda12-fasrc01
 module load Mambaforge
+export PATH=/n/home05/tinagong/.conda/envs/NNBP-OSD/bin:$PATH
 
 # Activate mamba env
 mamba activate myenv
@@ -20,4 +21,7 @@ nvidia-smi
 # Run the Python scripts with the provided parameters
 echo "Setup done. Starting training..."
 
+python3 train_segmentation.py --config configs/config_unet_ct_multi_att_dsv.json
+
+echo "First pass of model done. Starting training with early exit thresholds..."
 python3 train_segmentation.py --config configs/config_unet_ct_multi_att_dsv.json
